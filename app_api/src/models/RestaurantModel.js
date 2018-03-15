fb4d28d38728b33858d6fb6183c07851255d863b
dssalaza <dssalaza@gmail.com>
2018-03-14 21:32:13 -0500

    List restaurants - models, query & error message

---
+++ app_api/src/models/RestaurantModel.js
@@ -0,0 +1,11 @@
+//Js to module the restaurant table of the DB
+module.exports = {
+    fields:{
+        id          : "int",
+        nombre      : "text",
+        ubicacion   : "text",
+        ptj_general : "int",
+        ptj_plato   : "int",
+    },
+    key:["id"]
+}
--- app_api/src/routes/auth.js
+++ app_api/src/routes/auth.js
@@ -6,9 +6,9 @@ const router = express.Router()
 var models = require('express-cassandra')
 
 router.post('/login', (req, res) => {
-        
+
     const { email, password } = req.body.credentials
-    
+
     models.instance.Users.findOne({ email: email }, (err, user) => {
         if(err) {
             errors.errorDataBaseConnection(res)
@@ -16,13 +16,13 @@ router.post('/login', (req, res) => {
         }
         if(!user){
             errors.errorIncorrectEmail(res)
-            return 
+            return
         }
         if(user.comparePassword(password, user.password)){
-            res.json({ user: user.authJSON(user) });        
+            res.json({ user: user.authJSON(user) });
         }else{
             errors.errorIncorrectPassword(res)
-        } 
+        }
     });
 })
 
@@ -38,7 +38,7 @@ router.post('/register', (req, res) => {
         }
         if(user){
             errors.errorEmailIsRegistered(res)
-            return 
+            return
         }
         // Hash the password
         bcrypt.hash(password, 10, (err, encryptedPassword) => {
@@ -47,7 +47,7 @@ router.post('/register', (req, res) => {
                 return
             }
             // Add to database
-            // Create a model instance 
+            // Create a model instance
             var newUser = new models.instance.Users({
                 email: email,
                 password: encryptedPassword,
@@ -59,11 +59,36 @@ router.post('/register', (req, res) => {
                     errors.errorSavingUser(res)
                     return;
                 }
-                res.json({ user: "Registered user" });        
-            });    
+                res.json({ user: "Registered user" });
+            });
         });
 
     })
 })
+
+
+router.get('/restaurant/list', (res) => {
+
+  var query = {
+      //order the query by the ptj_general field in ascendent order
+      $orderby:{ '$asc' :'ptj_general' },
+      //limit the result set to 10 rows (enought data for our mock-up)
+      $limit: 10
+  }
+
+  models.instance.Person.find(query, {raw: true}, function(err, restaurant_list){
+      //restaurant_list is an array of plain objects satisfying the query conditions above
+      if(err) {
+          errors.errorDataBaseConnection(res)
+          return
+      }
+      if(!restaurant_list){
+          errors.errorIncorrectRestaurant(res)
+          return
+      }
+      res.json({ restaurant_list });    //If everything is ok then returns the restaurant_list
+  });
+
+}
 
 export default router;
